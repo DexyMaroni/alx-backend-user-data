1978ae08edbb0678657291bdde3123014d015cae
DexyMaroni <divineinyang7@gmail.com>
2024-05-03 01:12:29 +0000

    Authenticate

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,3 +9,21 @@ class Auth():
     """A class to manage API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns A False - path"""
+                if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for p in excluded_paths:
+            if p.endswith('*'):
+                if path.startswith(p[:1]):
+                    return False
+        return False if path in excluded_paths else True
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization header check"""
+        if request:
+            return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user method"""
+        return None
